876212a6447bdda71e66f1f66f5bae0c69d6bc30
Sven Olafsen <stockburner9@gmail.com>
2023-07-30 01:23:15 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    <h1>You're all set!</h1>
+    <div className="card">
+    <a href="fulfill://PaymentComplete">
+        Back to app
+      </a>
+      
+    </div>
+   
+  </>
+  )
+}
+
+export default App
