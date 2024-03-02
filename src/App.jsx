dc21925a01f8ac0ab1ae69d2ec3c7703a04f8919
Sven Olafsen <stockburner9@gmail.com>
2024-03-01 23:43:31 -0600

    removed deep link

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ function App() {
       
     <h1>You're all set!</h1>
     <div className="card">
-    <a href="fulfill://PaymentComplete">
+    {/* <a href="fulfill://PaymentComplete">
         Back to app
-      </a>
+      </a> */}
+
+      <p>Close this tab and continue back to the app</p>
       
     </div>
    
